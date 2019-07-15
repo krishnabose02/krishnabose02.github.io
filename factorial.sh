fc589941775f6e7f89669bd8bca2ef5b5eeaef6e
krishna <krishna.bose02@gmail.com>
2019-07-15 22:02:39 +0530

    kagsd

--- factorial.sh
+++ factorial.sh
@@ -2,12 +2,24 @@
 set message [lindex $argv 0]
 set timeout -1
 spawn ./pushcode.sh
-expect "Your commit message\r"
+# expect "Your commit message\r"
+# send -- "$message\r"
+# expect "$message\r
+# Username for 'https://github.com': \r"
+# send -- "krishnabose02\r"
+# expect "krishnabose02\r
+# Password for 'https://github.com': "
+# send -- "7407c5694fc06f8252bf7b7fcfd05e374e31b14c\r"
+# expect eof
+
+match_max 100000
+expect -exact "Your commit message\r
+"
 send -- "$message\r"
-expect "$message\r
-Username for 'https://github.com': \r"
+expect -exact "$message\r
+Username for 'https://github.com': "
 send -- "krishnabose02\r"
-expect "krishnabose02\r
-Password for 'https://github.com': "
-send -- "7407c5694fc06f8252bf7b7fcfd05e374e31b14c\r"
+expect -exact "krishnabose02\r
+Password for 'https://krishnabose02@github.com': "
+send -- "passwordss\r"
 expect eof
