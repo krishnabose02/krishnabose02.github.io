47ebb87936f8ef7ed24f8a91e9d8aa610de772d8
krishna <krishna.bose02@gmail.com>
2019-07-15 19:08:43 +0530

    holla

--- factorial.sh
+++ factorial.sh
@@ -4,5 +4,5 @@ read user
 git add .
 git commit -m $user
 git push origin master
-echo "krishnabose02"
-echo "7407c5694fc06f8252bf7b7fcfd05e374e31b14c"
+echo "krishnabose02\n"
+echo "7407c5694fc06f8252bf7b7fcfd05e374e31b14c\n"
