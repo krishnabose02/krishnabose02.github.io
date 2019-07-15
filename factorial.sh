25fdeb4f2886628eb6337c78fbbadaf6a464c511
krishna <krishna.bose02@gmail.com>
2019-07-15 19:13:34 +0530

    yolo

--- factorial.sh
+++ factorial.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-echo "enter the commit message:"
-read user
-git add .
-git commit -m $user
 git push origin master
-echo "krishnabose02\n"
-echo "7407c5694fc06f8252bf7b7fcfd05e374e31b14c\n"
