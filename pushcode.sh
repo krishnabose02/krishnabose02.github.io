43d8a5091ec0cc33561d0ce336b2f7539faadb82
krishna <krishna.bose02@gmail.com>
2019-07-15 21:49:49 +0530

    aiaaa

--- pushcode.sh
+++ pushcode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 echo "Enter commit message"
 read message
-echo "You have entered commit message :" $message
+# echo "You have entered commit message :" $message
 git add . &> /dev/null
 git commit -m "$message" &> /dev/null
 git push origin master
