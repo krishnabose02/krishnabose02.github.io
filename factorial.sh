0effb6df004024650127a32e157dc521ed7ae587
krishna <krishna.bose02@gmail.com>
2019-07-15 21:42:10 +0530

    ninth?

--- factorial.sh
+++ factorial.sh
@@ -4,8 +4,8 @@ set timeout -1
 spawn ./pushcode.sh
 expect "Enter commit message\r"
 send -- "$message\r"
-expect "Username for 'https://github.com': \r"
+expect "Username for 'https://github.com': "
 send -- "krishnabose02\r"
-expect "Password for 'https://github.com': \r"
+expect "Password for 'https://github.com': "
 send -- "7407c5694fc06f8252bf7b7fcfd05e374e31b14c\r"
 expect eof
